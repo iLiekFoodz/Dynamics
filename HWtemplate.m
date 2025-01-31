138f54ac6d31ade33ce35a6eac2025271e07782d
Foodzz <96932153+iLiekFoodz@users.noreply.github.com>
2025-01-31 09:31:36 -0500

    Add files via upload
    Template

---
+++ HWtemplate.m
@@ -0,0 +1,14 @@
+%=======================================================
+% MATLAB HW #1
+% Name: Nouh Shaikh
+% Date: 1/31/2025 
+%=======================================================
+
+%% Problem 1
+% some code that solves probem 1
+
+%% Problem 2
+
+% some code that solves probem 2
+
+% etc etc, just put 2 commenters next to each other to create a section
